cbdb5822172ba906c96c6c03c5325dd23029c3b6
menna <menna7hoss@gmail.com>
2022-10-14 15:44:26 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	l = n%10;
+	l = n % 10;
 	if (l > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
@@ -23,5 +23,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
-        return (0);
+return (0);
 }
