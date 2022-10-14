f83c5aa9523da10ed3a28ae8fc6489a9aa39f5d1
menna <menna7hoss@gmail.com>
2022-10-14 16:19:46 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+	char s;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (s = 'A'; s < 'Z'; s++)
+	{
+		putchar(s);
+	}
+	putchar('\n');
+return (0);
+}
