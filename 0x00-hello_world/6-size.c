73957b019b398a62bc02a52178f07e6df1b7e34e
menna <menna7hoss@gmail.com>
2022-10-13 17:38:28 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char c;
+        char c;
 	int i;
 	float f;
 	long int li;
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
-        printf("Size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
