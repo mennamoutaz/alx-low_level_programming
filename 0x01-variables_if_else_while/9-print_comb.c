933cb5776a75869fd83421e5cca7a4246799073d
menna <menna7hoss@gmail.com>
2022-10-15 14:53:03 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,12 +3,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 8; i++)
+	putchar ('0');
+	for (i = 1; i <= 9; i++)
 	{
-		putchar(i + '0');
 		putchar(',');
 		putchar(' ');
+		putchar(i + '0');
 	}
-	putchar(i + '0','\n');
 return (0);
 }
