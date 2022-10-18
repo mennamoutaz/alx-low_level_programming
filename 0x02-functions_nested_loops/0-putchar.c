9edad562f296fc6bb7eb6c0c47e656815b6cf119
menna <menna7hoss@gmail.com>
2022-10-18 13:55:21 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char ch[] = "_putchar";
+
+	while (i < 8)
+	{
+		int i = 0;
+
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
+return (0);
+}
