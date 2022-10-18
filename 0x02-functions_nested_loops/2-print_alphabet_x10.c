3b15bf560c6f387d7a8f538a41790e31b349c436
menna <menna7hoss@gmail.com>
2022-10-18 15:25:03 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,11 +8,16 @@
 void print_alphabet_x10 (void)
 {
 	int i;
+	char c;
 
 	i = 0;
 	while (i < 10)
 	{
-		print_alphabet();
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 		i++;
 	}
 return;
