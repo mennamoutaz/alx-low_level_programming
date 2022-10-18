4aeec9cfa48d5a7d3d26624597301f9f30983713
menna <menna7hoss@gmail.com>
2022-10-18 14:57:21 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ * Return: void
+ **/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	print_alphabet();
+return (0);
+}
