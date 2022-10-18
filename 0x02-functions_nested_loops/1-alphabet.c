ddda49c985cd1c7284ecd931b830aa3391cbb167
menna <menna7hoss@gmail.com>
2022-10-18 15:05:08 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdi.h>
 #include "main.h"
 /**
  * print_alphabet - prints lower case alphabet
@@ -13,14 +13,5 @@ void print_alphabet(void)
 	{
 		_putchar(c);
 	}
-}
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- **/
-int main(void)
-{
-	print_alphabet();
-return (0);
+return;
 }
