63b2c7bc3814dfe5c217e8184a31357479077b7b
menna <menna7hoss@gmail.com>
2022-10-18 15:16:11 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prrints thee alphabet 10 times
+ *
+ * Return: void
+ **/
+void print_alphabet_x10 (void)
+{
+	int i;
+	char c;
+
+	int i = 0;
+	while ( i < 10)
+	{
+		print_alphabet();
+	}
+return
